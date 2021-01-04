cf96ad881f5413c34cd81ea08ed6211329ae5b46
ElizaKnapp <eknapp20@stuy.edu>
2021-01-04 13:04:33 -0500

    piglatin 3 completed, still testing

--- PigLatin.java
+++ PigLatin.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class PigLatin {
   public static void main(String[] args) {
     //quick test pl 2
-    System.out.println(pigLatin("apple"));
-    System.out.println(pigLatin("banana"));
-    System.out.println(pigLatin("check"));
+    System.out.println(pigLatinBest("apple"));
+    System.out.println(pigLatinBest("banana"));
+    System.out.println(pigLatinBest("check"));
 
   }
 
@@ -23,11 +23,24 @@ public class PigLatin {
                           "tr", "tw", "wh", "wr"};
     String first2 = s.substring(0, 2);
     boolean containsDi = false;
-    for (int i = 0; i < digraphs.length; i++) 
+    for (int i = 0; i < digraphs.length; i++)
       if (first2.equals(digraphs[i])) containsDi = true;
     if (containsDi) return s.substring(2) + first2 + "ay";
     else return pigLatinSimple(s);
+  }
 
+  public static String pigLatinBest(String s) {
+    char first = s.charAt(0);
+    char last = s.charAt(s.length() - 1);
+    if (!((65 <= last && last <= 90) || (97 <= last && last <= 122) || (48 <= last && last <= 57))) {
+      char save = last;
+      s = s.substring(0, s.length() - 1);
+      return pigLatin(s) + save;
+    }
+
+    if (!((65 <= first && first <= 90) || (97 <= first && first <= 122)))
+      return s;
+    else return pigLatin(s);
   }
 
 }
